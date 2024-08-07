ac93e9a3a6412e21cdb9eceaf407d6a3a62f983c
zukas97 <zuercherlukas97@gmail.com>
2024-08-07 11:22:33 -0400

    opens file from the arg

--- src/main.c
+++ src/main.c
@@ -5,17 +5,28 @@
 //#include <unistd.h>
 
 bool running = false;
+char buffer[1024];
+int x;
+int y;
 
-void init() {
+void init(char * arg) {
+	x = 0;
+	y = 0;
 	printf("hello\n");
+	FILE *fptr;
+	fptr = fopen(arg, "r");
 	initscr();
+	while (fgets(buffer, sizeof(buffer), fptr)) {
+		printw(buffer, x, y);
+	}
 	running = true;
 }
 
-int main() {
+int main(int argc, char **argv) {
 	//printf("hello\n");
 	//initscr();
-	init();
+	char *arg = *argv;
+	init(arg);
 	while (running) {
 		char ch = getch();
 		//addch(ch);
